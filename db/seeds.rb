6cd55b64b2b1a9a084af57b559e574aa25184748
Adam Grandquist <grandquista@gmail.com>
2016-11-05 12:15:05 -0700

    Update column names in  seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveSupport::JSON.decode(File.read('localData/10.json')).each do |a|
+  Geoid2010.create!(a, without_protection: true)
+end
+
+# ActiveSupport::JSON.decode(File.read('localData/dataMap.txt')).each do |a|
+#   Demographic.create!(a, without_protection: true)
+# end
